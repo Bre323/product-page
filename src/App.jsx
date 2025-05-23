6065fd600eb0be68e3b6c9aa2550f5ef7d0b7f3f
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-05-23 14:10:05 -0300

    Set tailwind settings

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    </>
+    <div className="container">
+      <header className="bg-gray-800 w-full h-200"></header>
+      <aside className="bg-gray-700 w-full h-200"></aside>
+      <main className='bg-gray-600 w-full h-200'></main>
+      <footer className='bg-gray-500 w-full h-200'></footer>
+    </div>
   )
 }
 
