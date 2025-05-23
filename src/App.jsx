5b27ee05ce88d3563ce977206dd0ff5fcc6e6002
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2025-05-23 15:57:47 -0300

    set assets and customize header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="container">
-      <header className="bg-gray-800 w-full h-200"></header>
-      <aside className="bg-gray-700 w-full h-200"></aside>
-      <main className='bg-gray-600 w-full h-200'></main>
-      <footer className='bg-gray-500 w-full h-200'></footer>
+    <div className='font-sans min-w-4xl'>
+      <header className="bg-gray-800 text-gray-50 w-full h-20 flex justify-between align-center">
+        <h1 className='m-5 text-2xl font-bold'>Store</h1>
+
+        <nav className='m-6.5'>
+          <ul className='flex gap-8'>
+            <li className='hover:font-bold cursor-pointer'>Consoles</li>
+            <li className='hover:font-bold cursor-pointer'>Notebooks</li>
+            <li className='hover:font-bold cursor-pointer'>Acessórios</li>
+            <li className='hover:font-bold cursor-pointer'>Jogos</li>
+          </ul>
+        </nav>
+
+        <div className="flex m-6.5 gap-2">
+          <input className='bg-gray-700 rounded-md p-4' type="search" placeholder='Pesquisar'/>
+          <button className='bg-[url(./assets/icon-search.svg)] bg-no-repeat bg-center bg-size-4 bg-sky-700 rounded-md pl-10 pt-7.75 hover:bg-sky-600 cursor-pointer'/>
+        </div>
+      </header>
+
+      <main className='bg-gray-700 w-full h-200'></main>
+      <footer className='bg-gray-800 w-full h-30'></footer>
     </div>
   )
 }
